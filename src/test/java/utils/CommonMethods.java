5c9794600e4da3e08b7ac7106206ad18859ec6f1
alabizon <lyabizon@hotmail.com>
2023-04-07 12:20:12 -0700

    Groups started

--- src/test/java/utils/CommonMethods.java
+++ src/test/java/utils/CommonMethods.java
@@ -21,7 +21,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import stepDefinitions.assignGroupsWindowElements;
 import testBase.PageInitializer;
 
 public class CommonMethods extends PageInitializer {
